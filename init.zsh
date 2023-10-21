acf56be12565f1b4104fb2e2c5978e6fa6cd9b9a
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-10-21 12:24:19 -0300

    fix: update modules

--- init.zsh
+++ init.zsh
@@ -6,7 +6,7 @@ target_dir="$CURE_HOME/modules"
 func="${CURE_HOME}/functions"
 
 if [[ ! -d "$target_dir" ]]; then
-    printf "\ninstalling Cure in '$CURE_HOME'..."
+    echo "installing Cure in '$CURE_HOME'..."
     mkdir -p "$target_dir"
     source "$func/modules-init"
 fi
@@ -25,7 +25,7 @@ autoload -Uz -- mkcd mkpw coalesce git-action git-info
 
 for repo init_file in ${(kv)plugins}; do
     if [[ ! -d "$target_dir/$repo" ]]; then
-        printf "installing module: '$repo'"
+        echo "installing module: '$repo'"
        git clone "https://github.com/$repo" "$target_dir/$repo" > /dev/null 2>&1 
     fi
 
@@ -34,43 +34,28 @@ for repo init_file in ${(kv)plugins}; do
     fi
 done
 
-function git-pull-all-modules() {
-    if [[ -d "$target_dir" ]]; then
-        for repo init_file in ${(kv)plugins}; do
-            if [[ -d "$target_dir/$repo" ]]; then
-                printf "Updating $repo"
-                (cd "$target_dir/$repo" && git pull --quiet --ff --rebase --autostash)
-
-                source ${CURE_HOME}/init.zsh
-            fi
-        done
-    else
-        printf "Cure directory not found: $target_dir"
-    fi
-}
-
 function cure() {
     case "$1" in
          "version")
-            printf "Cure version: $VERSION" 
+            echo "Cure version: $VERSION" 
             ;;
         "update")
-            git-pull-all-modules
+            source "$func/modules-update" 
             ;;
         "upgrade")
             if [[ -d "$CURE_HOME" ]]; then
                 git -C "$CURE_HOME" pull --quiet --ff --rebase --autostash
-                printf "Cure self-update complete.\n"
+                echo "Cure self-update complete."
 
                 source ${CURE_HOME}/init.zsh
 
                 cure version
             else
-                printf "CURE_HOME directory not found: $CURE_HOME"
+                echo "CURE_HOME directory not found: $CURE_HOME"
             fi
             ;;
         *)
-            print "Usage: cure [update|upgrade|version]"
+            echo "Usage: cure [update|upgrade|version]"
             ;;
     esac
 }
